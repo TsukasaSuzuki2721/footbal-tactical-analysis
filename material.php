ddd39537cd055c442aa5e2d2d283f36446676383
鈴木束彩 <80496703+TsukasaSuzuki2721@users.noreply.github.com>
2021-03-13 00:38:44 +0900

    Add files via upload

---
+++ material.php
@@ -0,0 +1,100 @@
+<div class="detailBox" style="display: none;">
+  <div class="detailWrap">
+    <section class="profile">
+      <p class="number_profile" id="number"></p>
+      <p class="name_profile" id="name"></p>
+      <p class="sub-info">年齢 : <span id="age"></span>歳</p>
+      <p class="sub-info">身長 : <span id="height"></span></p>
+      <p class="sub-info">評価 : <span id="rating"></span></p>
+      <div class="close_detail">－</div>
+    </section>
+    <div class="detailList">
+      <section class="detail">
+        <p class="detailTitle">出場</p>
+        <table cellpadding="12px">
+          <tr>
+            <td class="detailName" align="left">出場</td>
+            <td class="detailValue" align="right"><span id="appearences"></span>回</td>
+          </tr>
+          <tr>
+            <td class="detailName" align="left">時間</td>
+            <td class="detailValue" align="right"><span id="minutes"></span>分</td>
+          </tr>
+        </table>
+      </section>
+      <section class="detail">
+        <p class="detailTitle">得点</p>
+        <table cellpadding="12px">
+          <tr>
+            <td class="detailName" align="left">ゴール</td>
+            <td class="detailValue" align="right"><span id="goals"></span>回</td>
+          </tr>
+          <tr>
+            <td class="detailName" align="left">アシスト</td>
+            <td class="detailValue" align="right"><span id="assists"></span>回</td>
+          </tr>
+        </table>
+      </section>
+      <section class="detail">
+        <p class="detailTitle">パス</p>
+        <table cellpadding="12px">
+          <tr>
+            <td class="detailName" align="left">総数</td>
+            <td class="detailValue" align="right"><span id="passes_total"></span>回</td>
+          </tr>
+          <tr>
+            <td class="detailName" align="left">成功率</td>
+            <td class="detailValue wrong-data" align="right"><span id="passes_suc"></span>%</td>
+          </tr>
+          <tr>
+            <td class="detailName" align="left">キーパス</td>
+            <td class="detailValue" align="right"><span id="passes_key"></span>回</td>
+          </tr>
+        </table>
+      </section>
+      <section class="detail">
+        <p class="detailTitle">ドリブル</p>
+        <table cellpadding="12px">
+          <tr>
+            <td class="detailName" align="left">総数</td>
+            <td class="detailValue" align="right"><span id="dribbles_total"></span>回</td>
+          </tr>
+          <tr>
+            <td class="detailName" align="left">成功率</td>
+            <td class="detailValue" align="right"><span id="dribbles_suc"></span>回</td>
+          </tr>
+        </table>
+      </section>
+      <section class="detail">
+        <p class="detailTitle">デュエル</p>
+        <table cellpadding="12px">
+          <tr>
+            <td class="detailName" align="left">総数</td>
+            <td class="detailValue" align="right"><span id="duels_total"></span>回</td>
+          </tr>
+          <tr>
+            <td class="detailName" align="left">勝率</td>
+            <td class="detailValue" align="right"><span id="duels_suc"></span>%</td>
+          </tr>
+        </table>
+      </section>
+      <section class="detail">
+        <p class="detailTitle">タックル</p>
+        <table cellpadding="12px">
+          <tr>
+            <td class="detailName" align="left">総数</td>
+            <td class="detailValue" align="right"><span id="tackles_total"></span>回</td>
+          </tr>
+          <tr>
+            <td class="detailName" align="left">成功数</td>
+            <td class="detailValue" align="right"><span id="tackles_suc"></span>回</td>
+          </tr>
+          <tr>
+            <td class="detailName" align="left">インターセプト</td>
+            <td class="detailValue" align="right"><span id="tackles_int"></span>回</td>
+          </tr>
+        </table>
+      </section>
+    </div>
+  </div>
+</div>
